8c3ce99e80acd0535a1009595f01e839ff840544
Amir Atias Hori <amiratiashuri@gmail.com>
2025-01-06 21:45:50 +0200

    Updated components and added crypto animation

--- Reademe.py
+++ Reademe.py
@@ -8,5 +8,12 @@
 # רק סביבה ביתית
 # pm2 start LiveCryptoPrice.py --interpreter "C:\Users\USER\AppData\Local\Programs\Python\Python312\python.exe"
 
+# העלה ל git
+#git add .
+#git commit -m "Description of changes"
+#git push
 
-#git remote add origin https://github.com/YOUR_USERNAME/REPOSITORY_NAME.git
+
+#ssh -i "AmirCs.pem" ubuntu@16.171.135.218
+
+#git clone https://Shbiro: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@github.com/Shbiro/CryptoSAFE.git
